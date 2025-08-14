b6dd2535a2f9c151b206883f8894bbd2b8c5cea9
Juan Andrés <juan.rivera7@udea.edu.co>
2025-08-13 21:37:18 -0500

    correccion prisma

--- lib/auth.ts
+++ lib/auth.ts
@@ -48,9 +48,7 @@ export const authOptions: NextAuthOptions = {
           return {
             id: user.id.toString(),
             email: user.email,
-            name: user.name,
             role: user.role,
-            trainerId: user.trainerId?.toString(),
             trainerName: user.trainer?.name
           }
         } catch (error) {
